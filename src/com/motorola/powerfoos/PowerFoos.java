e72c2d1e33027af8aa387d9aff7238f6c775083b
Gnanodharan Madhavan <a19949@motorola.com>
2012-11-09 10:01:49 -0800

    Added a sound to start game.

--- src/com/motorola/powerfoos/PowerFoos.java
+++ src/com/motorola/powerfoos/PowerFoos.java
@@ -5,20 +5,24 @@ import com.google.zxing.client.android.PreferencesActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class PowerFoos extends Activity {
     private static final String FOOS_EMAIL_ID = "foos_email_id";
+    private MediaPlayer mp;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_foos);
+
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.intro);
+        mp.start();
 
         Button start = (Button)findViewById(R.id.start);
         Button settings = (Button)findViewById(R.id.settings);
@@ -71,15 +75,16 @@ public class PowerFoos extends Activity {
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.activity_power_foos, menu);
-//        return true;
-//    }
-
-
     public void onStartClicked(View v) {
         Intent intent = new Intent (this,WelcomeScreen.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        if(mp!=null)
+            mp.release();
+        super.onDestroy();
     }
 }
